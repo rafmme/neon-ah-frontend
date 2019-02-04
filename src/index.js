4d747e18b60bdb73b3e3271a87ae76ac40ade743
Olusola Ajayi <olusola.ajayi@andela.com>
2019-02-04 14:35:13 +0100

    dev branch

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const Index = () => {
-    return <div>Welcome to Author's Haven!</div>;
-};
-
-ReactDOM.render(<Index />, document.getElementById("index"));
