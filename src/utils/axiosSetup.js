4888bd568c317ba8039bb5a2bfe81bf960bbf579
rafmme <rafmme@gmail.com>
2019-04-10 05:57:54 +0100

    Edit Axios

--- src/utils/axiosSetup.js
+++ src/utils/axiosSetup.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 export const apiInstance = axios.create({
-  baseURL: 'http://10.0.1.217/api/v1'
+    baseURL: 'http://localhost/api/v1'
 });
 
 const makeRequest = (url, options = { method: 'GET' }) => {
-  return apiInstance({
-    url,
-    method: options.method,
-    data: options.body,
-    headers: {
-      Accept: 'application/json',
-      'Content-type': 'application/json',
-      Authorization: localStorage.getItem('userToken') ? `Bearer ${localStorage.getItem('userToken')}` : ''
-    }
-  }).then(response => response.data);
+    return apiInstance({
+        url,
+        method: options.method,
+        data: options.body,
+        headers: {
+            Accept: 'application/json',
+            'Content-type': 'application/json',
+            Authorization: localStorage.getItem('userToken') ? `Bearer ${localStorage.getItem('userToken')}` : ''
+        }
+    }).then(response => response.data);
 };
 
 export default makeRequest;
